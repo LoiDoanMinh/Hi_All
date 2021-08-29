e9d7521d67c741f7ed8ad0a8f3eaf3b18cbe52c8
unknown <20521554@gm.uit.edu.vn>
2021-08-29 10:05:09 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x,S=0,i=0,n;
+ cout<<" xin moi nhap n :";cin>>n;
+ while(i<=n)
+ {
+     x=i*i;
+     S=S+x;
+     i++;
+
+ }
+ cout<<S;
+    return 0;
+}
